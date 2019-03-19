f3ca0f9898c5beb6492e16a05fb7a98777004493
Hung <nvhungiph@gmail.com>
2019-03-20 01:01:51 +0700

    feat: play audio from assets to keep folder's structure

--- android/src/main/java/com/johnsonsu/rnsoundplayer/RNSoundPlayerModule.java
+++ android/src/main/java/com/johnsonsu/rnsoundplayer/RNSoundPlayerModule.java
@@ -1,5 +1,6 @@
 package com.johnsonsu.rnsoundplayer;
 
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.net.Uri;
@@ -21,155 +22,143 @@ import com.facebook.react.bridge.Promise;
 
 public class RNSoundPlayerModule extends ReactContextBaseJavaModule {
 
-  public final static String EVENT_FINISHED_PLAYING = "FinishedPlaying";
-  public final static String EVENT_FINISHED_LOADING = "FinishedLoading";
+    public final static String EVENT_FINISHED_PLAYING = "FinishedPlaying";
+    public final static String EVENT_FINISHED_LOADING = "FinishedLoading";
 
-  private final ReactApplicationContext reactContext;
-  private MediaPlayer mediaPlayer;
+    private final ReactApplicationContext reactContext;
+    private MediaPlayer mediaPlayer;
 
-  public RNSoundPlayerModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNSoundPlayerModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNSoundPlayer";
-  }
+    @Override
+    public String getName() {
+        return "RNSoundPlayer";
+    }
 
-  @ReactMethod
-  public void playSoundFile(String name, String type) throws IOException {
-    mountSoundFile(name, type);
-    this.mediaPlayer.start();
-  }
+    @ReactMethod
+    public void playSoundFile(String name, String type) throws IOException {
+        mountSoundFile(name, type);
+        this.mediaPlayer.start();
+    }
 
-  @ReactMethod
-  public void loadSoundFile(String name, String type) throws IOException {
-    mountSoundFile(name, type);
-  }
+    @ReactMethod
+    public void loadSoundFile(String name, String type) throws IOException {
+        mountSoundFile(name, type);
+    }
 
-  @ReactMethod
-  public void playUrl(String url) throws IOException {
-    if (this.mediaPlayer == null) {
-      Uri uri = Uri.parse(url);
-      this.mediaPlayer = MediaPlayer.create(getCurrentActivity(), uri);
-      this.mediaPlayer.setOnCompletionListener(
-        new OnCompletionListener() {
-          @Override
-          public void onCompletion(MediaPlayer arg0) {
+    @ReactMethod
+    public void playUrl(String url) throws IOException {
+        if (this.mediaPlayer == null) {
+            Uri uri = Uri.parse(url);
+            this.mediaPlayer = MediaPlayer.create(getCurrentActivity(), uri);
+            this.mediaPlayer.setOnCompletionListener(
+                    new OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer arg0) {
+                            WritableMap params = Arguments.createMap();
+                            params.putBoolean("success", true);
+                            sendEvent(getReactApplicationContext(), EVENT_FINISHED_PLAYING, params);
+                        }
+                    });
+        } else {
+            Uri uri = Uri.parse(url);
+            this.mediaPlayer.reset();
+            this.mediaPlayer.setDataSource(getCurrentActivity(), uri);
+            this.mediaPlayer.prepare();
+        }
+        WritableMap params = Arguments.createMap();
+        params.putBoolean("success", true);
+        sendEvent(getReactApplicationContext(), EVENT_FINISHED_LOADING, params);
+        this.mediaPlayer.start();
+    }
+
+    @ReactMethod
+    public void pause() throws IllegalStateException {
+        if (this.mediaPlayer != null) {
+            this.mediaPlayer.pause();
+        }
+    }
+
+    @ReactMethod
+    public void resume() throws IllegalStateException {
+        if (this.mediaPlayer != null) {
+            this.mediaPlayer.start();
+        }
+    }
+
+    @ReactMethod
+    public void stop() throws IllegalStateException {
+        if (this.mediaPlayer != null) {
+            this.mediaPlayer.stop();
+        }
+    }
+
+    @ReactMethod
+    public void setVolume(float volume) throws IOException {
+        if (this.mediaPlayer != null) {
+            this.mediaPlayer.setVolume(volume, volume);
+        }
+    }
+
+    @ReactMethod
+    public void getInfo(
+            Promise promise) {
+        WritableMap map = Arguments.createMap();
+        map.putDouble("currentTime", this.mediaPlayer.getCurrentPosition() / 1000.0);
+        map.putDouble("duration", this.mediaPlayer.getDuration() / 1000.0);
+        promise.resolve(map);
+    }
+
+    private void sendEvent(ReactApplicationContext reactContext,
+                           String eventName,
+                           @Nullable WritableMap params) {
+        reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+
+    private void mountSoundFile(String name, String type) throws IOException {
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+
+            this.mediaPlayer.setOnCompletionListener(
+                    new OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer arg0) {
+                            WritableMap params = Arguments.createMap();
+                            params.putBoolean("success", true);
+                            sendEvent(getReactApplicationContext(), EVENT_FINISHED_PLAYING, params);
+                        }
+                    });
+        }
+        mediaPlayer.reset();
+        AssetFileDescriptor descriptor = getReactApplicationContext().getAssets().openFd(name + "." + type);
+        if (descriptor.getFileDescriptor() != null) {
+            mediaPlayer.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+            descriptor.close();
+            mediaPlayer.prepare();
+
             WritableMap params = Arguments.createMap();
             params.putBoolean("success", true);
-            sendEvent(getReactApplicationContext(), EVENT_FINISHED_PLAYING, params);
-          }
-      });
-    } else {
-      Uri uri = Uri.parse(url);
-      this.mediaPlayer.reset();
-      this.mediaPlayer.setDataSource(getCurrentActivity(), uri);
-      this.mediaPlayer.prepare();
-    }
-    WritableMap params = Arguments.createMap();
-    params.putBoolean("success", true);
-    sendEvent(getReactApplicationContext(), EVENT_FINISHED_LOADING, params);
-    this.mediaPlayer.start();
-  }
-
-  @ReactMethod
-  public void pause() throws IllegalStateException {
-    if (this.mediaPlayer != null) {
-      this.mediaPlayer.pause();
-    }
-  }
-
-  @ReactMethod
-  public void resume() throws IllegalStateException {
-    if (this.mediaPlayer != null) {
-      this.mediaPlayer.start();
-    }
-  }
-
-  @ReactMethod
-  public void stop() throws IllegalStateException {
-    if (this.mediaPlayer != null) {
-      this.mediaPlayer.stop();
-    }
-  }
-
-  @ReactMethod
-  public void setVolume(float volume) throws IOException {
-    if (this.mediaPlayer != null) {
-      this.mediaPlayer.setVolume(volume, volume);
-    }
-  }
-
-  @ReactMethod
-  public void getInfo(
-      Promise promise) {
-    WritableMap map = Arguments.createMap();
-    map.putDouble("currentTime", this.mediaPlayer.getCurrentPosition() / 1000.0);
-    map.putDouble("duration", this.mediaPlayer.getDuration() / 1000.0);
-    promise.resolve(map);
-  }
-
-  private void sendEvent(ReactApplicationContext reactContext,
-                       String eventName,
-                       @Nullable WritableMap params) {
-    reactContext
-        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-        .emit(eventName, params);
-  }
-
-  private void mountSoundFile(String name, String type) throws IOException {
-    if (this.mediaPlayer == null) {
-      int soundResID = getReactApplicationContext().getResources().getIdentifier(name, "raw", getReactApplicationContext().getPackageName());
-
-      if (soundResID > 0) {
-        this.mediaPlayer = MediaPlayer.create(getCurrentActivity(), soundResID);
-      } else {
-        this.mediaPlayer = MediaPlayer.create(getCurrentActivity(), this.getUriFromFile(name, type));
-      }
-
-      this.mediaPlayer.setOnCompletionListener(
-        new OnCompletionListener() {
-          @Override
-          public void onCompletion(MediaPlayer arg0) {
-            WritableMap params = Arguments.createMap();
-            params.putBoolean("success", true);
-            sendEvent(getReactApplicationContext(), EVENT_FINISHED_PLAYING, params);
-          }
-      });
-    } else {
-      Uri uri;
-      int soundResID = getReactApplicationContext().getResources().getIdentifier(name, "raw", getReactApplicationContext().getPackageName());
-
-      if (soundResID > 0) {
-        uri = Uri.parse("android.resource://" + getReactApplicationContext().getPackageName() + "/raw/" + name);
-      } else {
-        uri = this.getUriFromFile(name, type);
-      }
-
-      this.mediaPlayer.reset();
-      this.mediaPlayer.setDataSource(getCurrentActivity(), uri);
-      this.mediaPlayer.prepare();
+            sendEvent(getReactApplicationContext(), EVENT_FINISHED_LOADING, params);
+        }
     }
 
-    WritableMap params = Arguments.createMap();
-    params.putBoolean("success", true);
-    sendEvent(getReactApplicationContext(), EVENT_FINISHED_LOADING, params);
-  }
+    private Uri getUriFromFile(String name, String type) {
+        String folder = getReactApplicationContext().getFilesDir().getAbsolutePath();
+        String file = name + "." + type;
 
-  private Uri getUriFromFile(String name, String type) {
-    String folder = getReactApplicationContext().getFilesDir().getAbsolutePath();
-    String file = name + "." + type;
+        // http://blog.weston-fl.com/android-mediaplayer-prepare-throws-status0x1-error1-2147483648
+        // this helps avoid a common error state when mounting the file
+        File ref = new File(folder + "/" + file);
 
-    // http://blog.weston-fl.com/android-mediaplayer-prepare-throws-status0x1-error1-2147483648
-    // this helps avoid a common error state when mounting the file
-    File ref = new File(folder + "/" + file);
+        if (ref.exists()) {
+            ref.setReadable(true, false);
+        }
 
-    if (ref.exists()) {
-      ref.setReadable(true, false);
+        return Uri.parse("file://" + folder + "/" + file);
     }
-
-    return Uri.parse("file://" + folder + "/" + file);
-  }
 }
